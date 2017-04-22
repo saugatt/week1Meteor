12b97f1fc72370bfd700304d701fe514bd99d36e
Saugat Tripathi <tripathi.saugat@gmail.com>
2017-04-22 13:44:30 -0400

    Added bootstrap

---
+++ client/main.js
@@ -0,0 +1,42 @@
+console.log("I am a client");
+var img_data =[ {
+  img_src: "saugat.jpg",
+  img_alt: "Saugats Head Shot"
+},
+{
+  img_src: "saugat.jpg",
+  img_alt: "Saugats Head Shot"
+},
+{
+  img_src: "saugat.jpg",
+  img_alt: "Saugats Head Shot"
+}
+];
+
+Template.images.helpers({images:img_data});
+
+/*
+
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+*/
---
+++ server/main.js
@@ -0,0 +1,10 @@
+console.log("I am a server");
+
+/*import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // console.log("I am a server);
+
+});
+
+*/
